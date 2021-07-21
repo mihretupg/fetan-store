48e4ae372611d668f290097563b47a1cfcb062ac
mihretupg <86960134+mihretupg@users.noreply.github.com>
2021-07-21 16:32:57 +0300

    Initial commit

---
+++ src/components/views/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="header">
+      <div className="container text-center">
+        <h3>Fetan Grocery</h3>
+        <p>Enjoy the fast shopping cart experience from internet</p>
+      </div>
+    </div>
+  );
+};
